7ef48b8f05eaa03a97842c087f6b73930d4fac1d
danilo.haertzer@googlemail.com <danilo.haertzer@googlemail.com@70de001c-9c6e-11dd-979b-47109e308e52>
2011-06-29 09:08:05 +0000

    fixed "NPE" on parsing RDF/XML with an empty literal
    git-svn-id: http://rdfquery.googlecode.com/svn/trunk@194 70de001c-9c6e-11dd-979b-47109e308e52

--- jquery.rdf.xml.js
+++ jquery.rdf.xml.js
@@ -388,7 +388,7 @@
               }
             }
           } else if (hasAttributeNS(p, rdfNs, 'datatype')) {
-            o = p.childNodes[0].nodeValue;
+            o = p.childNodes[0] ? p.childNodes[0].nodeValue : "";
             object = $.rdf.literal(o, { datatype: getAttributeNS(p, rdfNs, 'datatype') });
           } else if (p.getElementsByTagName('*').length > 0) {
             for (j = 0; j < p.childNodes.length; j += 1) {
